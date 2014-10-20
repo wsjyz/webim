7a5b89afeddf197241fbfb2b48c2898aef35087f
Lingfei-Zhang <javagoshell@gmail.com>
2014-10-20 14:30:29 +0800

    代码重构

--- public/static/javascripts/chat.client.js
+++ public/static/javascripts/chat.client.js
@@ -1,243 +1,151 @@
-/**
- * Created by dam on 2014/9/13.
- */
-
-var msg_template = '<p><span class="msg-block"><strong></strong><span class="time"></span><span class="msg"></span></span></p>';
-var widget_chat = $('.widget-chat');
-var messages = $('#chat-messages');
-var message_box = $('.chat-message');
-var message_box_input = $('.chat-message #msg-div');
-var messages_inner = $('#chat-messages-inner');
-var htmlEleId = 0;
-var isRegTriggerClickMedio = false;
-var webrtc;
-var userId = new Date().getTime();
-var room = window.location.hash.slice(1);
-if(!room){
-    room = "default"
-}
-$(function(){
-    bootbox.prompt("请输入昵称", function(result) {
-        if (result === null) {
-            return false;
+$(function () {
+    var messageInner = $(".webim-message-box-innner");
+    var onlineUsers = $(".webim-online-users");
+    var messageSendDiv = $(".webim-message-send-div");
+    var messageBox = $(".webim-message-box");
+    var count = 0;
+    var socket;
+    var userId = new Date().getTime();
+    var userPic = "/static/img/av2.jpg";
+    var room = window.location.hash.slice(1);
+    if (!room) {
+        room = "default"
+    }
+    bootbox.prompt("What is your name?", function (result) {
+        if (result === null || $.trim(result) == "") {
+            bootbox.alert("用户信息丢失,请重新打开网页!");
         } else {
-            joinRoom(result,room,userId);
+            joinRoom(userId, result, room,userPic);
+
         }
     });
-});
 
-function StringBuffer() {
-    this._strs = new Array;
-}
-StringBuffer.prototype.append = function (str) {
-    this._strs.push(str);
-};
-StringBuffer.prototype.toString = function() {
-    return this._strs.join("");
-};
+    function joinRoom(userId, userName, room,userPic) {
+        socket = io('http://localhost:3002/');
+        socket.on("connection_quit", function (data) {
+            if(data.quit) {
+                bootbox.alert(data.msg);
+            }else{
+                //发送加入事件
+                socket.emit('join_room', { userId: userId, userName: userName, roomId: room, userPic:userPic});
+                //注册各种事件监听
+                socket.on("broadcast_join_room", function(data){
+                    var userList = data.userList;
+                    var listContent = new StringBuffer();
+                    onlineUsers.empty();
+                    for(var i = 0; i < userList.length; i++) {
+                        var user = userList[i];
+                        listContent.append('<li id="'+user.userId+'"><img src="'+user.userPic+'" />'+user.userName+'</li>');
+                    }
+                    onlineUsers.append(listContent.toString());
+                });
 
-var socket;
+                $("#webim_message_send_btn").click(function() {
+                    var content = messageSendDiv.html();
+                    socket.emit('broadcast_send_text_msg', {content:content});
+                    addMessage({user:{userPic:userPic,userName:userName},content:content,time:getTimeStr()},true);
+                    messageSendDiv.html("").focus();
+                });
 
-function joinRoom(name,room,userId){
-    $('#userName').val(name);
-    socket = io('http://localhost:3002/');//192.168.1.61 115.29.47.23 webim.izhuangyuan.cn
-    socket.emit('join_room', {userName:name, roomId:room, userId:userId});
-    socket.on('broadcast_join_room', function(data){//监听加入事件
-        var mediaType = data.mediaType;
-        if(mediaType){
-            if(isRegTriggerClickMedio == false){
-                isRegTriggerClickMedio = true;
-                doPanelMessage(mediaType, data.panelMessage);
+                //监听聊天的消息接收
+                socket.on('broadcast_send_text_msg', function(data){
+                    addMessage(data,false);
+                });
+                //监听退出
+                socket.on("broadcast_quit_room",function(data) {
+                    userLeave(data);
+                    addTip("用户" + data.userName + "离开了");
+                });
+
+
+
             }
-        }
-        var userList = data.userList;
-        if(userList){
-            $('.contact-list').empty();
-            var list_content = new StringBuffer();
+        });
 
-            for(var i = 0;i < userList.length;i++){
-                if(userList[i].userName != '' && userList[i].userName != null){
-                    list_content.append('<li id="user-'+userList[i].socketId+'" class="online">' +
-                        '<a  class="pull-left"><img alt="" src="/static/images/av2.jpg" />' +
-                        ' <span>'+userList[i].userName+'</span></a>' +
-                        '</a>' +
-                        '<div class="clearfix"></div></li>');
-                }
-            }
-            $('.contact-list').append(list_content.toString());
-        }
+    }
 
-    });
-    socket.on('broadcast_send_text_msg', function(msg){//监听聊天事件
-        add_message(msg.userName+':','/static/images/av2.jpg',msg.content,true);
-    });
-    socket.on('broadcast_send_blob_msg', function(msg){//监听聊天事件
-        add_message(msg.userName+':','/static/images/av2.jpg','<img src="' + msg.blob_message + '"/>',true);
-    });
-    socket.on('broadcast_quit_room', function(msg){//监听聊天事件
-        if(msg){
-            remove_user(msg.userId,msg.userName);
-        }
-    });
-    socket.on('broadcast_join_rtc_room', function (msg) {
-        //监听加入音视频
-        if(isRegTriggerClickMedio == false){
-            isRegTriggerClickMedio = true;
-            doPanelMessage(msg.mediaType, msg.panelMessage);
+    function addMessage(data, isSelf){
+        count+=1;
+        if(isSelf){
+            var htmlMessageElement = '<div id="msg-'+count+'" class="webim-message-inner-left"><img class="webim-user-head-pic"  src="' +
+                data.user.userPic + '" />' +
+                '<span class="webim-triangle-left"></span><div class="webim-message"><h6> ' + '你' +
+                ' <span class="time">' +data.time+ '</span></h6>' + data.content + '</div></div>';
+            messageInner.append(htmlMessageElement);
+            $('#msg-'+count).fadeOut(0).fadeIn(500);
         }else{
-            $(".panel-message-content").html( msg.panelMessage);
+            var htmlMessageElement = '<div id="msg-'+count+'" class="webim-message-inner-left"><img class="webim-user-head-pic"  src="' + data.user.userPic + '" />' +
+                '<span class="webim-triangle-left"></span><div class="webim-message"><h6> ' + data.user.userName +
+                ' <span class="time">' +data.time+ '</span></h6>' + data.content + '</div></div>';
+            messageInner.append(htmlMessageElement);
+            $('#msg-'+count).fadeOut(0).fadeIn(500);
         }
-        add_notice_message(msg.user.userName, msg.mediaType=="video"?"进入视频聊天":"进入音频聊天");
-    });
-
-}
-//发送消息
-$('#sendBtn').click(function(){
-    var msgDiv = $('#msg-div');
-    var htmlMsg = msgDiv.html();
-    if(htmlMsg != ''){
-        socket.emit('broadcast_send_text_msg', {content:htmlMsg});
-    } else {
-        $('.input-box').addClass('has-error');
+        messageBox.animate({ scrollTop: messageInner.height() },500);
     }
-});
 
-messages.niceScroll({
-    zindex: 1060
-});
-
-message_box_input.keypress(function(e){
-    if($(this).val() != '') $('.input-box').removeClass('has-error');
-    if(e.which == 13) {
-        if($(this).val() != ''){
-            socket.emit('broadcast_send_text_msg', {content:$(this).val()});
-        } else {
-            $('.input-box').addClass('has-error');
-        }
-    }
-});
-
-function add_message(name,img,msg,clear) {
-
-    htmlEleId = htmlEleId + 1;
-
-    var time = new Date();
-    var hours = time.getHours();
-    var minutes = time.getMinutes();
-    var seconds = time.getSeconds();
-    if(hours < 10) hours = '0' + hours;
-    if(minutes < 10) minutes = '0' + minutes;
-    if(seconds<10)seconds = "0" + seconds;
-    var id = 'msg-'+htmlEleId;
-    var idname = name.replace(' ','-').toLowerCase();
-    messages_inner.append('<p id="'+id+'" class="user-'+idname+'"><img class="head-pic" src="'+img+'" alt="" />'
-        +'<span class="msg-block"><strong>'+name+'</strong> <span class="time">- '+hours+':'+minutes+":"+seconds+'</span>'
-        +'<span class="msg">'+msg+'</span></span></p>');
-    $('#'+id).fadeOut(0).addClass('show');
-    if(clear) {
-        $('.input-box').removeClass('has-error');
-        message_box_input.html('').focus();
+    function userLeave(data){
+        $("#"+data.userId).slideUp(500,function() {
+            $(this).remove();
+        });
 
     }
-    messages.animate({ scrollTop: messages_inner.height() },1000);
 
-    messages.getNiceScroll().resize();
-}
-
-function remove_user(userid,name) {
-    $('.contact-list li#user-'+userid).addClass('offline').delay(1000).slideUp(800,function(){
-        $(this).remove();
-    });
-    add_notice_message(name,"离开了");
-}
-
-function add_notice_message(name,eventName) {
-    htmlEleId = htmlEleId + 1;
-    var id = 'msg-'+htmlEleId;
-    messages_inner.append('<p class="offline al" id="'+id+'"><span>用户 <a >@'+name+'</a> '+eventName+'</span></p>');
-    $('#'+id).fadeOut(0).addClass('show');
-    messages.animate({ scrollTop: messages_inner.height() },1000);
-    messages.getNiceScroll().resize();
-}
-
-function pasteClob(evt){
-    //for chrome
-    var clipboardData = evt.clipboardData;
-    for(var i=0; i<clipboardData.items.length; i++){
-        var item = clipboardData.items[i];
-        if(item.kind=='file'&&item.type.match(/^image\//i)){
-            //blob就是剪贴板中的二进制图片数据
-            var blob = item.getAsFile(),reader = new FileReader();
-            //定义fileReader读取完数据后的回调
-            reader.onload=function(){
-                var sHtml='<img src="'+event.target.result+'">';//result应该是base64编码后的图片
-                  message_box_input.append($(sHtml));
-                //socket.emit('broadcast_send_blob_msg', event.target.result);
-            }
-            reader.readAsDataURL(blob);//用fileReader读取二进制图片，完成后会调用上面定义的回调函数
-        }
+    function addTip(tip){
+        count += 1;
+        htmlMessageElement = '<div id="msg-'+count+'" class="webim-message-inner-left"><div class="webim-message-tip">'+tip+'</div></div>';
+        messageInner.append(htmlMessageElement);
+        $('#msg-'+count).fadeOut(0).fadeIn(500);
+        messageBox.animate({ scrollTop: messageInner.height() },500);
     }
-}
 
-$("#joinVideoBtn").click(function(){
-    $(".invite-div").hide();
-    $(this).prop("disabled", true).addClass("disabled");
-    $("#joinAudioBtn").prop("disabled", true).addClass("disabled");
-    webrtc = new SimpleWebRTC({
-        localVideoEl: 'localVideo',
-        remoteVideosEl: 'remoteVideos',
-        autoRequestMedia: true
-    });
-    webrtc.on('readyToCall', function () {
-        // you can name it anything
-        webrtc.joinRoom(room);
-        socket.emit("broadcast_join_rtc_room",{mediaType:"video"})
-    });
-});
-
-$("#joinAudioBtn").click(function(){
-    $(".invite-div").hide();
-    $(this).prop("disabled", true).addClass("disabled");
-    $("#joinVideoBtn").prop("disabled", true).addClass("disabled");
-    webrtc = new SimpleWebRTC({
-        localVideoEl: 'localVideo',
-        remoteVideosEl: 'remoteVideos',
-        autoRequestMedia: true,
-        media: {
-            video: false,
-            audio: true
-        }
-    });
-    webrtc.on('readyToCall', function () {
-        webrtc.joinRoom(room);
-        socket.emit("broadcast_join_rtc_room",{mediaType:"audio"})
-    });
-});
-$("#chatRecordBtn").click(function(){
-    var pThis = $(this);
-    var pChatRecord = $("#chatRecord");
-    if(pThis.hasClass("active")){
-        pThis.removeClass("active");
-        pChatRecord.removeClass("chat-record");
-    }else{
-        pThis.addClass("active");
-        pChatRecord.addClass("chat-record");
+    function getTimeStr(){
+        var time = new Date();
+        var hours = time.getHours();
+        var minutes = time.getMinutes();
+        var seconds = time.getSeconds();
+        if(hours < 10) hours = '0' + hours;
+        if(minutes < 10) minutes = '0' + minutes;
+        if(seconds<10)seconds = "0" + seconds;
+        return hours+":"+minutes+":"+seconds;
     }
+
+    function StringBuffer() {
+        this._strs = new Array;
+    }
+    StringBuffer.prototype.append = function (str) {
+        this._strs.push(str);
+    };
+    StringBuffer.prototype.toString = function() {
+        return this._strs.join("");
+    };
 
 });
 
-function doPanelMessage(mediaType, panelMessage){
-    $(".panel-message-content").html(panelMessage);
-    if(!$("#localVideo").prop("src")){
-        $(".invite-div").show(1000);
-        if(mediaType == "video"){
-            $(".panelMessageBtn").click(function(){
-                $("#joinVideoBtn").trigger("click");
-            });
-        }else{
-            $(".panelMessageBtn").click(function(){
-                $("#joinAudioBtn").trigger("click");
-            })
-        }
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
